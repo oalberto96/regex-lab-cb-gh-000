461b214153cb926a05a2801e50a7d8d350dfda2d
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-24 23:28:12 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,7 +10,7 @@ end
 
 def words_five_letters_long(text)
   list = text.scan(/ [a-z]{5}\b/)
-  list.join.strip
+  puts list.join.strip
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
