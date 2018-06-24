7ddbe82a9aa62e0497f938803ea2ce811f22a08e
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-24 23:45:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,5 +18,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone =~ /\d+/ ? true : false
+  result = phone =~ /\d+/ ? true : false
+  binding.pry
+  result
 end
