a48652c7067f9043e9cd67f8c429ede2427db315
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-24 23:48:22 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word =~ /^[aeiouAEIOU][a-z]+/ ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/ [a-z]{5}\b/).join.split
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text =~ /([A-Z]\s+.)+/ ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone =~ /^(\(\d+\)\d+)|(\d)$/ ? true : false
 end
