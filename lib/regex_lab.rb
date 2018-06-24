1a75dd578cc04ca84e86c3e0e85bf1ec87695ac1
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-24 23:33:58 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  result = word =~ /[A-Z]\w+./ ? true : false
+  result = text =~ /[A-Z]\w+./ ? true : false
+  binding.pry
 end
 
 def valid_phone_number?(phone)
