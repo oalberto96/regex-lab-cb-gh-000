b5964526255a9cddd499fabeea4629c42a2a58df
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-24 22:11:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,11 +6,10 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/un\w*ing/)
-  binding.pry
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\S{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
